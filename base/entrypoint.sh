3068ccbec8f476f8103cd340a024916336cc7edb
Julian <julianho@brandeis.edu>
2021-03-02 13:14:52 +0000

    Create base image for running Fido simulation

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,91 +1,34 @@
 #!/bin/bash
 
-if [[ ! -z "${GW_IP}" ]]; then
-    echo "setting up routes for tailscale-relay"
-    ip route del default
-    ip route add 100.64.0.0/10 via $RELAY_IP
-    ip route add default via $GW_IP
-else
-    # Setup tun interface for tailscale
-    echo "setting up for running tailscale locally"
-    if [ ! -d /dev/net ]; then mkdir /dev/net; fi
-    if [ ! -e /dev/net/tun ]; then  mknod /dev/net/tun c 10 200; fi
-fi
+echo "checking openGl support"
+glxinfo | grep '^direct rendering:'
 
-echo "starting sshd"
-/etc/init.d/ssh restart
+echo "starting window manager jwm"
+jwm &
 
-# Startup.sh (copied from fcwu/docker-ubuntu-vnc-desktop)
-if [ -n "$VNC_PASSWORD" ]; then
-    echo -n "$VNC_PASSWORD" > /.password1
-    x11vnc -storepasswd $(cat /.password1) /.password2
-    chmod 400 /.password*
-    sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
-    export VNC_PASSWORD=
-fi
+echo "starting x11"
+/usr/bin/Xtigervnc \
+    -desktop "x11" \
+    -localhost \
+    -rfbport 5900 \
+    -SecurityTypes None \
+    -AlwaysShared \
+    -AcceptKeyEvents \
+    -AcceptPointerEvents \
+    -AcceptSetDesktopSize \
+    -SendCutText \
+    -AcceptCutText \
+    :99 &
 
-if [ -n "$X11VNC_ARGS" ]; then
-    sed -i "s/^command=x11vnc.*/& ${X11VNC_ARGS}/" /etc/supervisor/conf.d/supervisord.conf
-fi
+echo "starting noVNC"
+/usr/local/bin/easy-novnc \
+    --addr :6080 \
+    --host localhost \
+    --port 5900 \
+    --no-url-password \
+    --basic-ui \
+    --novnc-params "resize=remote" &
 
-if [ -n "$OPENBOX_ARGS" ]; then
-    sed -i "s#^command=/usr/bin/openbox.*#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
-fi
+DISPLAY=:99 xterm
 
-if [ -n "$RESOLUTION" ]; then
-    sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
-fi
-
-USER=${USER:-root}
-HOME=/my_ros_data
-if [ "$USER" != "root" ]; then
-    echo "* enable custom user: $USER"
-    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
-    if [ -z "$PASSWORD" ]; then
-        echo "  set default password to \"ubuntu\""
-        PASSWORD=ubuntu
-    fi
-    HOME=/home/$USER
-    echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME}
-    chown -R $USER:$USER ${HOME}
-    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
-fi
-sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
-
-# home folder
-if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
-    mkdir -p $HOME/.config/pcmanfm/LXDE/
-    ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
-    chown -R $USER:$USER $HOME
-fi
-
-# nginx workers
-sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
-
-# nginx ssl
-if [ -n "$SSL_PORT" ] && [ -e "/etc/nginx/ssl/nginx.key" ]; then
-    echo "* enable SSL"
-	sed -i 's|#_SSL_PORT_#\(.*\)443\(.*\)|\1'$SSL_PORT'\2|' /etc/nginx/sites-enabled/default
-	sed -i 's|#_SSL_PORT_#||' /etc/nginx/sites-enabled/default
-fi
-
-# nginx http base authentication
-if [ -n "$HTTP_PASSWORD" ]; then
-    echo "* enable HTTP base authentication"
-    htpasswd -bc /etc/nginx/.htpasswd $USER $HTTP_PASSWORD
-	sed -i 's|#_HTTP_PASSWORD_#||' /etc/nginx/sites-enabled/default
-fi
-
-# dynamic prefix path renaming
-if [ -n "$RELATIVE_URL_ROOT" ]; then
-    echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
-	sed -i 's|#_RELATIVE_URL_ROOT_||' /etc/nginx/sites-enabled/default
-	sed -i 's|_RELATIVE_URL_ROOT_|'$RELATIVE_URL_ROOT'|' /etc/nginx/sites-enabled/default
-fi
-
-# clearup
-PASSWORD=
-HTTP_PASSWORD=
-
-exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
+source /opt/ros/melodic/setup.bash
--- base/files/rootfs/.bashrc
+++
@@ -1,4 +0,0 @@
-echo "[running ~/.bashrc]"
-source ~/rosutils/common_alias.bash
-# export ROBOT_IP=xx.xx.xx.xx 
-rset cloud
